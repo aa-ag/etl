839b71978ecde27918ec3e58c9000cf251cfd179
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-16 18:07:09 -0500

    authenticated service account, created BigQuery client and enabled API in Google Cloud

--- script.py
+++ script.py
@@ -6,11 +6,32 @@ import requests
 import pandas as pd
 from pandas.io.json import json_normalize
 import settings
+import json
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
 ############------------ GLOBAL VARIABLE(S) ------------############
-url = settings.end_point
+url = settings.gbfs_end_point
+path_to_bigquery_key = settings.bigquery_account_key_path
+
+json_file = open('etlproject.json', 'r')
+api_credentials = json.load(json_file)
 
 ############------------ FUNCTION(S) ------------############
+def big_query_authentication():
+    '''
+     set credentials from etlproject.json (key imported from Google Cloud)
+     sets client using bigquery import object `Client`
+    '''
+    credentials = service_account.Credentials.from_service_account_file(
+        path_to_bigquery_key, scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id,)
+
+    print(client)
+
+
 def test_endpoint():
     '''
      creates and makes a request, 
@@ -27,5 +48,7 @@ def test_endpoint():
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    print(test_endpoint())
+    # print(test_endpoint())
     # <Response [200]>
+    big_query_authentication()
+    # <google.cloud.bigquery.client.Client object at 0x7fb442242f40>
