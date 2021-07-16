da3f0c9bfc6475f1c9791ad5ad300413402af392
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-16 18:13:06 -0500

    authenticated user, BigQuery credentials working, sample query working and returning expected results

--- script.py
+++ script.py
@@ -14,10 +14,40 @@ from google.oauth2 import service_account
 url = settings.gbfs_end_point
 path_to_bigquery_key = settings.bigquery_account_key_path
 
-json_file = open('etlproject.json', 'r')
-api_credentials = json.load(json_file)
 
 ############------------ FUNCTION(S) ------------############
+def query_stackoverflow():
+    '''
+     sample code from docs to test connection
+     modified to use credentials
+     https://cloud.google.com/bigquery/docs/quickstarts/quickstart-client-libraries
+    '''
+    
+    credentials = service_account.Credentials.from_service_account_file(
+        path_to_bigquery_key, scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id,)
+
+    query_job = client.query(
+        """
+        SELECT
+          CONCAT(
+            'https://stackoverflow.com/questions/',
+            CAST(id as STRING)) as url,
+          view_count
+        FROM `bigquery-public-data.stackoverflow.posts_questions`
+        WHERE tags like '%google-bigquery%'
+        ORDER BY view_count DESC
+        LIMIT 3"""
+    )
+
+    results = query_job.result()  # Waits for job to complete.
+
+    for row in results:
+        print("{} : {} views".format(row.url, row.view_count))
+
+
 def big_query_authentication():
     '''
      set credentials from etlproject.json (key imported from Google Cloud)
@@ -50,5 +80,11 @@ def test_endpoint():
 if __name__ == "__main__":
     # print(test_endpoint())
     # <Response [200]>
-    big_query_authentication()
+    # big_query_authentication()
     # <google.cloud.bigquery.client.Client object at 0x7fb442242f40>
+    query_stackoverflow()
+    '''
+    https://stackoverflow.com/questions/35159967 : 98375 views
+    https://stackoverflow.com/questions/22879669 : 93709 views
+    https://stackoverflow.com/questions/10604135 : 92234 views
+    '''
