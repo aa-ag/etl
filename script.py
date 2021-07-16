ffd343e7d8736c63fc86742fd013ec2c45ebc8eb
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-15 19:11:12 -0500

    added settings import, setting url with enpoint and created both test_endpoint function as well as driver code function

--- script.py
+++ script.py
@@ -5,11 +5,20 @@ import datetime
 import requests
 import pandas as pd
 from pandas.io.json import json_normalize
+import settings
 
 ############------------ GLOBAL VARIABLE(S) ------------############
-
+url = settings.end_point
 
 ############------------ FUNCTION(S) ------------############
+def test_endpoint():
+    '''
+     creates and makes a request, 
+     returns whether or not connection is successful
+    '''
+    pass
 
 
 ############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    test_endpoint()
